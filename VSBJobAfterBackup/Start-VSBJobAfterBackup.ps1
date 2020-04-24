385d61730add263a2e092c8aef386037b414e6ea
feoktist9696 <feoktist9696@gmail.com>
2020-04-24 22:15:56 +0300

    Create Start-VSBJobAfterBackup.ps1

---
+++ VSBJobAfterBackup/Start-VSBJobAfterBackup.ps1
@@ -0,0 +1,40 @@
+asnp VeeamPSSnapin
+function Start-VSBJobAfterBackup {
+[CmdletBinding()]
+param(
+    [Parameter(
+        Position = 0,
+        Mandatory = $true
+    )]
+    [Veeam.Backup.Core.SureBackup.CSbJob[]]$Job,
+    [int]$retries,
+    [int]$waitS
+
+)
+$Count = 0
+$success = $null
+
+do {
+    try {
+       $sessions = Get-VBRBackupSession | where {$_.State -ne 'Stopped'}
+       if ($sessions -eq $null) {
+        $sbJobs = $Job
+        foreach ($sbJob in $sbJobs) {
+            write-host 'Starting '$sbJob
+            Start-VSBJob -Job $sbJob
+        }
+        $success = $true
+       }
+       elseif ($sessions -ne $null) {
+            write-host 'Some backup jobs are still running. Sleeping for'$waitS 'seconds'
+            Start-Sleep -Seconds $waitS
+            $Count += 1
+            write-host 'Retry '$Count
+       }
+}
+catch {
+    throw $_.Exception
+}
+}
+until ($Count -eq $retries -or $Success)
+} 
