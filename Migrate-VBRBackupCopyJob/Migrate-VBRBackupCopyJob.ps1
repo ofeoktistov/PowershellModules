197a72bbdece11369858c271cc313a21703a7dc5
feoktist9696 <feoktist9696@gmail.com>
2020-10-25 23:55:48 +0300

    Create Migrate-VBRBackupCopyJob.ps1

---
+++ Migrate-VBRBackupCopyJob/Migrate-VBRBackupCopyJob.ps1
@@ -0,0 +1,41 @@
+ Function Migrate-VBRBackupCopyJob {
+[CmdletBinding()]
+
+param (
+   [Parameter(ParameterSetName='SourceJob', Mandatory=$false,
+              HelpMessage="Name of the source job that you want to migrate to immediate mode")]
+   [Veeam.Backup.Core.CBackupJob]$SourceJob,
+   [Parameter(ParameterSetName='ApplyToAll', Mandatory=$false,
+              HelpMessage="Enable if you want to migrate all backup copy jobs to immediate mode")]
+   [Switch]$ApplyToAll
+
+
+)
+try {
+    if ($ApplyToAll) {
+        $SourceJob = Get-VBRJob | where {$_.JobType -eq 'BackupSync'}
+    }
+foreach ($source in $SourceJob) {
+    $linkedJobs = @()
+    foreach ($linkedJob in $sourceJob.LinkedJobIds) {
+        $job = $jobs | where {$_.Id -eq $linkedJob}
+        $linkedJobs += $job
+
+    }
+    $targetRepository = $sourceJob.FindTargetRepository()
+    $sourceWan = $sourceJob.FindSourceWanAccelerator()
+    $targetWan = $sourceJob.FindTargetWanAccelerator()
+    $migratedName = "$($sourceJob.Name)_migrated"
+ 
+    Add-VBRViBackupCopyJob -Name $migratedName -Description $sourcejob.Description -BackupJob $linkedJobs -SourceAccelerator $sourceWan -TargetAccelerator $targetWan -Repository $targetRepository -EnableImmediateCopy
+    $migratedJob = Get-VBRJob -Name $migratedName
+    $options = Get-VBRJobOptions -Job $sourceJob
+    Set-VBRJobOptions -Job $migratedJob -Options $options
+  
+}
+}
+catch {
+ Write-Host "An error occured."
+ Write-Host $_
+}
+} 
