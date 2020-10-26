26baae063156d05a69a14691a7ac5f5edfe3724b
feoktist9696 <feoktist9696@gmail.com>
2020-10-26 15:10:06 +0300

    Update Migrate-VBRBackupCopyJob.ps1

--- Migrate-VBRBackupCopyJob/Migrate-VBRBackupCopyJob.ps1
+++ Migrate-VBRBackupCopyJob/Migrate-VBRBackupCopyJob.ps1
@@ -1,10 +1,10 @@
-function Migrate-VBRBackupCopyJob {
+ function Migrate-VBRBackupCopyJob {
 [CmdletBinding()]
 
 param (
    [Parameter(ParameterSetName='SourceJob', Mandatory=$false,
               HelpMessage="Name of the source job that you want to migrate to immediate mode")]
-   [Veeam.Backup.Core.CBackupJob]$SourceJob,
+   [Veeam.Backup.Core.CBackupJob[]]$SourceJob,
    [Parameter(ParameterSetName='ApplyToAll', Mandatory=$false,
               HelpMessage="Enable if you want to migrate all backup copy jobs to immediate mode")]
    [Switch]$ApplyToAll
@@ -22,26 +22,51 @@ foreach ($source in $SourceJob) {
     }
     else {
     $linkedJobs = @()
-    foreach ($linkedJob in $sourceJob.LinkedJobIds) {
-        $job = $jobs | where {$_.Id -eq $linkedJob}
+    foreach ($linkedJob in $source.LinkedJobIds) {
+        $job = Get-VBRJob | where {$_.Id -eq $linkedJob}
         $linkedJobs += $job
 
     }
-    $targetRepository = $sourceJob.FindTargetRepository()
-    $sourceWan = $sourceJob.FindSourceWanAccelerator()
-    $targetWan = $sourceJob.FindTargetWanAccelerator()
-    $migratedName = "$($sourceJob.Name)_migrated"
- 
-    Add-VBRViBackupCopyJob -Name $migratedName -Description $sourcejob.Description -BackupJob $linkedJobs -SourceAccelerator $sourceWan -TargetAccelerator $targetWan -Repository $targetRepository -EnableImmediateCopy
-    $migratedJob = Get-VBRJob -Name $migratedName
-    $options = Get-VBRJobOptions -Job $sourceJob
-    Set-VBRJobOptions -Job $migratedJob -Options $options
-  
-}
-}
-}
+    $wanEnabled = $source.IsWanAcceleratorEnabled()
+    $description = $source.Description
+    $targetRepository = $source.FindTargetRepository()
+    $migratedName = "$($source.Name)_migrated"
+    if ($wanEnabled -eq $true -and $description -eq "") {
+        $sourceWan = $source.FindSourceWanAccelerator()
+        $targetWan = $source.FindTargetWanAccelerator()
+        Add-VBRViBackupCopyJob -Name $migratedName -BackupJob $linkedJobs -SourceAccelerator $sourceWan -TargetAccelerator $targetWan -Repository $targetRepository -EnableImmediateCopy
+        $migratedJob = Get-VBRJob -Name $migratedName
+        $options = Get-VBRJobOptions -Job $source
+        Set-VBRJobOptions -Job $migratedJob -Options $options
+     }
+    
+    elseif ($wanEnabled -eq $false -and $description -ne "") {
+        Add-VBRViBackupCopyJob -Name $migratedName -BackupJob $linkedJobs -Description $source.Description -DirectOperation -Repository $targetRepository -EnableImmediateCopy
+        $migratedJob = Get-VBRJob -Name $migratedName
+        $options = Get-VBRJobOptions -Job $source
+        Set-VBRJobOptions -Job $migratedJob -Options $options
+    
+    }
+    elseif ($wanEnabled -eq $true -and $description -ne "") {
+        $sourceWan = $source.FindSourceWanAccelerator()
+        $targetWan = $source.FindTargetWanAccelerator()
+        Add-VBRViBackupCopyJob -Name $migratedName -BackupJob $linkedJobs -Description $source.Description -SourceAccelerator $sourceWan -TargetAccelerator $targetWan -Repository $targetRepository -EnableImmediateCopy
+        $migratedJob = Get-VBRJob -Name $migratedName
+        $options = Get-VBRJobOptions -Job $source
+        Set-VBRJobOptions -Job $migratedJob -Options $options
+    }
+    else {
+        Add-VBRViBackupCopyJob -Name $migratedName -BackupJob $linkedJobs -DirectOperation -Repository $targetRepository -EnableImmediateCopy
+        $migratedJob = Get-VBRJob -Name $migratedName
+        $options = Get-VBRJobOptions -Job $source
+        Set-VBRJobOptions -Job $migratedJob -Options $options
+     
+    }
+    }
+   }
+ }
 catch {
  throw $_
-}
+ }
 
-} 
+}  
